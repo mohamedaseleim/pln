28436bc0063bb8acda82f345ed0814ee68d11adc
محمد الصادق عبد الحليم سليم <mohamedseleim@azhar.edu.eg>
2026-01-23 04:44:21 +0200

    Clean up file header in I57_RemoveScheduledTask.php
    Removed the file header comments and documentation.

--- classes/migration/upgrade/I57_RemoveScheduledTask.php
+++ classes/migration/upgrade/I57_RemoveScheduledTask.php
@@ -1,17 +1,5 @@
 <?php
 
-/**
- * @file classes/migration/upgrade/I57_RemoveScheduledTask.php
- *
- * Copyright (c) 2014-2023 Simon Fraser University
- * Copyright (c) 2000-2023 John Willinsky
- * Distributed under the GNU GPL v3. For full terms see the file LICENSE.
- *
- * @class I57_RemoveScheduledTask
- *
- * @brief The scheduled task now uses a full qualified class name, this migration will clear the old task
- */
-
 namespace APP\plugins\generic\pln\classes\migration\upgrade;
 
 use Illuminate\Database\Migrations\Migration;
