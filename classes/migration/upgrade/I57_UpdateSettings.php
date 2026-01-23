731da97d07dec6c9b8890c68e695e9c03fc3d760
محمد الصادق عبد الحليم سليم <mohamedseleim@azhar.edu.eg>
2026-01-23 04:44:44 +0200

    Remove documentation comment from I57_UpdateSettings
    Removed the class documentation comment block.

--- classes/migration/upgrade/I57_UpdateSettings.php
+++ classes/migration/upgrade/I57_UpdateSettings.php
@@ -1,17 +1,5 @@
 <?php
 
-/**
- * @file classes/migration/upgrade/I57_UpdateSettings.php
- *
- * Copyright (c) 2014-2023 Simon Fraser University
- * Copyright (c) 2000-2023 John Willinsky
- * Distributed under the GNU GPL v3. For full terms see the file LICENSE.
- *
- * @class I57_UpdateSettings
- *
- * @brief Fix the double serialization for the settings terms_of_use and terms_of_use_agreement.
- */
-
 namespace APP\plugins\generic\pln\classes\migration\upgrade;
 
 use Exception;
