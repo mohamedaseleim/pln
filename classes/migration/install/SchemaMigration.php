8ffb8f0810ad6ab915b73c1de8a8f8384e67a4ef
محمد الصادق عبد الحليم سليم <mohamedseleim@azhar.edu.eg>
2026-01-23 04:41:23 +0200

    Clean up SchemaMigration.php header comments
    Removed the header comments and adjusted closing brace.

--- classes/migration/install/SchemaMigration.php
+++ classes/migration/install/SchemaMigration.php
@@ -1,17 +1,5 @@
 <?php
 
-/**
- * @file classes/migration/install/SchemaMigration.php
- *
- * Copyright (c) 2014-2023 Simon Fraser University
- * Copyright (c) 2000-2023 John Willinsky
- * Distributed under the GNU GPL v3. For full terms see the file LICENSE.
- *
- * @class SchemaMigration
- *
- * @brief Describe database table structures.
- */
-
 namespace APP\plugins\generic\pln\classes\migration\install;
 
 use APP\plugins\generic\pln\classes\migration\upgrade\I28_FixDepositStatus;
