2d8441ec192f2b5e3d6b092f530483e52d55437b
محمد الصادق عبد الحليم سليم <mohamedseleim@azhar.edu.eg>
2026-01-23 04:32:18 +0200

    Remove documentation comments from StatusGridRow.php
    Removed class-level documentation comments from StatusGridRow.php.

--- controllers/grid/StatusGridRow.php
+++ controllers/grid/StatusGridRow.php
@@ -1,17 +1,5 @@
 <?php
 
-/**
- * @file controllers/grid/StatusGridRow.php
- *
- * Copyright (c) 2014-2023 Simon Fraser University
- * Copyright (c) 2000-2023 John Willinsky
- * Distributed under the GNU GPL v3. For full terms see the file LICENSE.
- *
- * @class StatusGridRow
- *
- * @brief Handle PLNStatus deposit grid row requests.
- */
-
 namespace APP\plugins\generic\pln\controllers\grid;
 
 use APP\plugins\generic\pln\classes\deposit\Deposit;
