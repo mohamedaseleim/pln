3ed185721ad5a5d0101cb0d6e597a7a6c4b129b3
محمد الصادق عبد الحليم سليم <mohamedseleim@azhar.edu.eg>
2026-01-23 04:43:37 +0200

    Remove documentation comments from upgrade migration file
    Removed file-level documentation comments from I28_FixDepositStatus.php.

--- classes/migration/upgrade/I28_FixDepositStatus.php
+++ classes/migration/upgrade/I28_FixDepositStatus.php
@@ -1,17 +1,5 @@
 <?php
 
-/**
- * @file classes/migration/upgrade/I28_FixDepositStatus.php
- *
- * Copyright (c) 2014-2023 Simon Fraser University
- * Copyright (c) 2000-2023 John Willinsky
- * Distributed under the GNU GPL v3. For full terms see the file LICENSE.
- *
- * @class I28_FixDepositStatus
- *
- * @brief Adds new fields to manage the deposit status and resets the status for all deposits.
- */
-
 namespace APP\plugins\generic\pln\classes\migration\upgrade;
 
 use Illuminate\Database\Migrations\Migration;
